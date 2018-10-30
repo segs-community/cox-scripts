251c7c051d531203a026eb14af55a57408b7be64
ZopharShinta <43687491+ZopharShinta@users.noreply.github.com>
2018-10-29 22:57:54 -0500

    Updated to support name of plaque/location

--- Outbreak Examples/plaques.lua
+++ Outbreak Examples/plaques.lua
@@ -1,9 +1,9 @@
 
-plaque_visited = function(id, loc)
+plaque_visited = function(name, loc)
 
-    printDebug("Plaque Id: " .. tostring(id) .. " X: " .. round2(loc.x, 4) .. " Y: " .. round2(loc.y, 4) .. " Z: " .. round2(loc.z, 4))
+    printDebug("Plaque name: " .. name .. " Loc X: " .. round2(loc.x, 4) .. " Y: " .. round2(loc.y, 4) .. " Z: " .. round2(loc.z, 4))
     vec3Ptest = vec3.new(round2(loc.x, 4),round2(loc.y, 4),round2(loc.z, 4))
     Character.faceLocation(client.m_ent, vec3Ptest)
-  
+    
     return ""
 end
